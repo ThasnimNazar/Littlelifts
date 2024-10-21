8d0b9a7ef9f672b7cb75a6dc7615dbd4877bbe4b
ThasnimNazar <www.thachunazar@gmail.com>
2024-10-21 21:37:25 +0530

    vite config configured

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -18,6 +18,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     nodePolyfills()
